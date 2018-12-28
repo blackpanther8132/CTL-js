012a13b03abfe959e83d32d556a39b27818bf555
blackpanther8132 <blackpanther8132@gmail.com>
2018-12-28 16:30:12 +0530

    git commit

--- single_all.js
+++ single_all.js
@@ -1,3 +1,23 @@
+var animal = function(name)
+{
+    this.name=name;
+    throw new Error("cant creat instance for it")
+}
+animal.prototype.details = function()
+{
+    return "this is" +this.name;
+}
+var tiger = function(type){
+    this.type=type;
+
+}
+tiger.prototype = new animal("rabbit");
+var tg=new tiger("good");
+console.log(tg.name);
+
+// person
+
+
 var person= function(name)
 {
 this.name=name;
@@ -30,6 +50,40 @@ console.log(gp.quality);
 console.log(bp.getname());
 console.log(bp.namelength());
 console.log(bp.quality);
+
+// books
+
+var books = function(){
+
+}
+books.prototype.details = function()
+{
+    console.log("action");
+}
+var action = function(){
+   
+}
+action.prototype=new books();
+action.prototype.details= function()
+{
+    console.log("300 w");
+}
+var romantic = function(){
+}
+romantic.prototype=new books();
+romantic.prototype.details =function()
+{
+    console.log(" titanic ");
+}
+var harror = function()
+{
+    
+}
+harror.prototype = new books();
+
+
+console.log(action.details);
+
 
 
 var books = function(){
