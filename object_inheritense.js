22b1ffcd8170b1705cf28c01f7592b87ad2f3d6b
blackpanther8132 <blackpanther8132@gmail.com>
2018-12-28 12:51:06 +0530

    git commit

---
+++ object_inheritense.js
@@ -0,0 +1,35 @@
+var person= function(name)
+{
+this.name=name;
+}
+person.prototype.getname= function()
+{
+        return this.name;
+}
+person.prototype.namelength = function()
+{
+    return this.name.length;
+}
+
+var goodperson = function(quality)
+{
+    this.quality=quality;
+}
+var badperson = function(quality)
+{
+    this.quality=quality;
+}
+//var pr=new person("raju");
+//var goodperson.prototype=pr;
+goodperson.prototype=new person("Raju");
+badperson.prototype=new person("raaaa");
+var gp = new goodperson("smile");
+var bp= new badperson("angry");
+console.log(gp.getname());
+console.log(gp.namelength());
+console.log(gp.quality);
+console.log(bp.getname());
+console.log(bp.namelength());
+console.log(bp.quality);
+
+
