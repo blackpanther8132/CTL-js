9f78f574bcc908b59014eb5a4099c15e4593db0c
blackpanther8132 <blackpanther8132@gmail.com>
2018-12-31 11:32:28 +0530

    git commit

--- object1.js
+++ object1.js
@@ -5,14 +5,14 @@ function game(name,rank)
 }
 function sport(name1,cat)
 {
-    this.name1=name1;
+    this.name=name1;
     this.cat=cat;
-    this.game=game;
+    this.game=game;// it is like importing game function into sport function
 
 }
 var mysport=new sport("cricket","wk");
 mysport.game("kabaddi",1);
-console.log(mysport.name1);
+console.log(mysport.name);
 console.log(mysport.cat);
 console.log(mysport.name);
 
